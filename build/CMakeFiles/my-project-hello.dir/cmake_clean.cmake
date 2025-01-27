3d8f32ac3a078d4286a2e857cd078d8b1d356a5e
Andrew Bickford <111834095+abick2@users.noreply.github.com>
2025-01-26 22:08:08 -0500

    My first commit

---
+++ build/CMakeFiles/my-project-hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my-project-hello.dir/main.cpp.o"
+  "CMakeFiles/my-project-hello.dir/main.cpp.o.d"
+  "my-project-hello"
+  "my-project-hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my-project-hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
